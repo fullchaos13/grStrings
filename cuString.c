db4e1a97c7404187085ce27d2732c979ad0d5168
Gleb Reznicov <reznicovgleb@gmail.com>
2016-01-16 10:52:28 -0500

    Version 0.0: Initial version of grStrings (currently named as cuStrings). Currently supports 5 functions, with limited flexibility and various untested cases.

---
+++ cuString.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "cuString.h"
+
+#define BUFFER_SIZE 256
+
+char* getUserString(){
+
+ char* tempString;
+ char temp = '\0';
+ int counter = 0;
+
+    if(!malloc(BUFFER_SIZE + 1)){
+        printf("Error: malloc fail.\n");
+        exit(EXIT_FAILURE);
+    }else{
+        tempString = malloc(BUFFER_SIZE + 1);
+    }
+
+    for(char* i = tempString; i < tempString + BUFFER_SIZE; i++){
+
+        scanf("%c", &temp);
+        if(temp != '\n'){
+            *i = temp;
+            counter++;
+        }else{
+            *i = '\0';
+            counter++;
+            break;
+        }
+    }
+
+    realloc(tempString, counter);
+
+    return tempString;
+}
+
+
+int getStringSize(char* a){
+
+ int counter = 0;
+
+    for(;; a++){
+
+        if(*a != '\0'){
+            counter++;
+        }else{
+            break;
+        }
+    }
+    return counter;
+}
+
+
+char* copyString(char* a){
+
+ char* b;
+
+    if(!malloc(getStringSize(a) + 1)){
+        printf("Error: malloc fail.\n");
+        exit(EXIT_FAILURE);
+    }
+    b = malloc(getStringSize(a) + 1);
+
+    for(int i = 0; i < getStringSize(a) + 1; i++){
+
+        *(b + i) = *(a + i);
+
+    }
+    return b;
+}
+
+int stringToInt(char* a){
+
+ int num = 0, negativeFlag = 0, size = 0;
+
+    size = getStringSize(a);
+
+    if(*a == '-'){
+        size--;
+        negativeFlag = 1;
+    }
+
+    for(char* i = a + negativeFlag; size > 0; i++){
+
+        if((*i <= '9') && (*i >= '0')){
+            num += (*i - 48) * (int)pow(10.0, (float)size - 1.0);
+            size--;
+        }else{
+            printf("Error: [%c] is not a valid digit.\n", *i);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if(negativeFlag == 1){
+       num *= -1;
+    }
+
+    return num;
+}
+
+
+void printString(char* a){
+
+    for(char* i = a; *i != '\0'; i++){
+        printf("%c", *i);
+    }
+}
+
+
---
+++ cuString.h
@@ -0,0 +1,44 @@
+#ifndef CUSTRING_H
+
+/*****************************
+* getUserString: Dynamically *
+* allocates storage for a    *
+* string of 512 bytes, then  *
+* removes any empty remaining*
+* bytes after user input.    *
+* returns string without any *
+* wasted space.              *
+*****************************/
+char* getUserString();
+
+/*****************************
+* getStringSize: Returns the *
+* size of the string         *
+* excluding the NULL char.   *
+*****************************/
+int getStringSize(char* a);
+
+/*****************************
+* copyString: Returns a copy *
+* of the parametized string. *
+*****************************/
+char* copyString(char* a);
+
+/*****************************
+* stringToInt: Returns       *
+* parametized string as int. *
+* Program terminates if      *
+* the string is not a valid  *
+* integer.                   *
+*****************************/
+int stringToInt(char* a);
+
+/*****************************
+* printString: Prints the    *
+* dynamically allocated      *
+* string on the console      *
+* window.                    *
+*****************************/
+void printString(char* a);
+
+#endif // CUSTRING_H
